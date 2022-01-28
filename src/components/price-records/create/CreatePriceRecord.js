1d148f229c436798adf7f04bdf50cf3c6a3de8b6
Rob Monhemius <robmonhemius@gmail.com>
2022-01-28 03:14:22 +0100

    improved workflow

--- src/components/price-records/create/CreatePriceRecord.js
+++ src/components/price-records/create/CreatePriceRecord.js
@@ -1,15 +1,17 @@
 import { Button } from '@wordpress/components';
-import { useState } from '@wordpress/element'
+import { useState, createRef } from '@wordpress/element'
 import { __ } from '@wordpress/i18n';
 import './CreatePriceRecord.css';
 
 function CreatePriceRecord( props ) {
 
     const [record, set_record] = useState( { name: '', price: '1.00' } );
+    const name_input_ref = createRef();
 
     function emit(){
         props.onEmit( {...record, price: +record.price} );
         set_record( { name: '', price: '1.00' } );
+        name_input_ref.current.focus();
     }
 
     function set_name( event ) {
@@ -23,7 +25,7 @@ function CreatePriceRecord( props ) {
     }
 
     return ( <form className='create-price-record'>
-        <input type='text' className='name' value={record.name} onChange={set_name} />
+        <input ref={name_input_ref} type='text' className='name' value={record.name} onChange={set_name} />
         <input type='number' className='price' value={record.price} onChange={set_price} />
         <Button variant="secondary" type="button" onClick={emit}>{ __('Insert', 'price-list-block-zebra') }</Button>
     </form> );
