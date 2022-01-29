067d2898a26c28a2ebe7a1abda41a8ade02c0dfb
Rob Monhemius <robmonhemius@gmail.com>
2022-01-29 01:48:50 +0100

    wrap words

--- src/components/price-records/read/ReadPriceRecord.js
+++ src/components/price-records/read/ReadPriceRecord.js
@@ -2,12 +2,20 @@
 import { __ } from '@wordpress/i18n';
 
 function ReadPriceRecord( props ){
+    const name = props.record.name.split( ' ' );
+    const name_jsx = name.map( (word, index) => {
+        if( name.length === index + 1 ) 
+            return <span className='word last-word'>{word}</span>;
+        else
+            return <span className='word'>{word}&nbsp;</span>;
+    });
+
     return <div class="record-row">
         <div class='name'>
-            <span>{ props.record.name }</span>
+            { name_jsx }
             <span class='spacer'></span>
         </div>
-        <div class='price'>&euro;{ props.record.price }</div>
+        <div class='price'>{ props.record.price }</div>
     </div>;
 }
 
