5aa91f7478acfa9b1d9964fbcc19141848c9d8bb
Rob Monhemius <robmonhemius@gmail.com>
2022-03-15 10:28:22 +0100

    refactor: modular css

--- src/components/edit/edit.js
+++ src/components/edit/edit.js
@@ -3,23 +3,8 @@ import UpdatePriceRecord from '../price-records/update/UpdatePriceRecord';
 
 // @see https://developer.wordpress.org/block-editor/packages/packages-i18n/
 import { __ } from '@wordpress/i18n';
+import style from './editor.module.scss';
 
-/**
- * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
- * Those files can contain any CSS code that gets applied to the editor.
- *
- * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
- */
-import './editor.scss';
-
-/**
- * The edit function describes the structure of your block in the context of the
- * editor. This represents what the editor will render when the block is used.
- *
- * @see https://developer.wordpress.org/block-editor/developers/block-api/block-edit-save/#edit
- *
- * @return {WPElement} Element to render.
- */
 export default function Edit({ attributes, setAttributes }) {
 
 	function add_record( record ) {
@@ -39,7 +24,7 @@ export default function Edit({ attributes, setAttributes }) {
 		setAttributes( { price_records } );
 	}
 
-	function move_record_down( index, direction ) {
+	function move_record_down( index ) {
 		const price_records = [...attributes.price_records];
 		const [record] = price_records.splice( index, 1 );
 		price_records.splice( index + 1, 0, record  );
@@ -47,7 +32,7 @@ export default function Edit({ attributes, setAttributes }) {
 		setAttributes( { price_records } );
 	}
 
-	function move_record_up( index, direction ) {
+	function move_record_up( index ) {
 		const price_records = [...attributes.price_records];
 		const [record] = price_records.splice( index, 1 );
 		price_records.splice( index - 1, 0, record  );
@@ -56,10 +41,10 @@ export default function Edit({ attributes, setAttributes }) {
 	}
 
 	return ( <div>
-		<div className='price-record-top-labels'>
-			<div className="name">{ __( 'Item / Service', 'price-list-block-zebra' ) }</div>
-			<div className="price">{ __( 'Price', 'price-list-block-zebra' ) }</div>
-			<div className="action"></div>
+		<div className={style['price-record-top-labels']}>
+			<div className={style.name}>{ __( 'Item / Service', 'price-list-block-zebra' ) }</div>
+			<div className={style.price}>{ __( 'Price', 'price-list-block-zebra' ) }</div>
+			<div className={style.action}></div>
 		</div>
 		{ attributes.price_records.map( (record, index) => <UpdatePriceRecord key={record.id} move_down={move_record_down} move_up={move_record_up} onDelete={delete_record} onUpdate={update_record} index={index} total_records={attributes.price_records.length} record={ record } /> ) }
 		<CreatePriceRecord onEmit={add_record} latest_id={ attributes.price_record_latest_index } />
