e5a3db32f00a6464eaa776de351acc69be110034
Rob Monhemius <robmonhemius@gmail.com>
2022-03-18 06:25:09 +0100

    refactor: moved price

--- src/components/price-records/update/UpdatePriceRecord.js
+++ src/components/price-records/update/UpdatePriceRecord.js
@@ -1,4 +1,3 @@
-import { useState } from '@wordpress/element';
 import css from './UpdatePriceRecord.module.scss';
 import DeletePriceRecord from '../delete/DeletePriceRecord';
 import OrderButton from './order-button/OrderButton';
@@ -6,15 +5,6 @@ import DescriptionInput from './description-input/DescriptionInput';
 import PriceInput from './price-input/PriceInput';
 
 function UpdatePriceRecord( props ) {
-    const [price, set_price] = useState( props.record.price );
-    
-    function process_changes(){
-        if( price === props.record.price ) return;
-
-        const formatted_price = Number( price ).toFixed(2);
-        props.onUpdate( { ...props.record, price: formatted_price } );
-        set_price( Number( price ).toFixed(2) );
-    }
 
     function remove() {
         props.onDelete( props.record );
@@ -26,8 +16,10 @@ function UpdatePriceRecord( props ) {
         props.onUpdate( {...props.record, name: value } );
     }
 
-    function update_price( event ) {
-        set_price( event.target.value );
+    function update_price( value ) {
+        if( value === props.record.price ) return;
+
+        props.onUpdate( { ...props.record, price: value } );
     }
 
     function move_down() {
@@ -38,10 +30,10 @@ function UpdatePriceRecord( props ) {
         props.move_up( props.index );
     }
 
-    return (<div className={css['update-price-record']} onBlur={process_changes} > 
+    return (<div className={css['update-price-record']} > 
         <OrderButton display={props.settings.order_items } move_down={move_down} move_up={move_up}  enable_move_up={props.index > 0} enable_move_down={props.total_records > props.index + 1} />
         <DescriptionInput editable={props.settings.edit_description} value={ props.record.name } onChange={update_name} focus={ props.index === 0 && props.settings.edit_description } />
-        <PriceInput editable={props.settings.edit_price} value={price} onChange={update_price} />
+        <PriceInput editable={props.settings.edit_price} value={props.record.price} onChange={update_price} />
         <DeletePriceRecord display={props.settings.delete} onEmit={remove} ></DeletePriceRecord>
     </div>)
 }
--- src/components/price-records/update/price-input/PriceInput.js
+++ src/components/price-records/update/price-input/PriceInput.js
@@ -1,9 +1,25 @@
 import css from './PriceInput.module.scss';
+import { useState, useEffect } from '@wordpress/element';
 
 export default function PriceInput( props ) {
+
+    const [ value, set_value ] = useState( props.value );
+
+    useEffect( () => {
+        props.onChange( Number( value ).toFixed(2) );
+    }, [ value ]);
+
+    function onChange( event ) {
+        set_value( event.target.value );
+    }
+
+    function onBlur( event ) {
+        set_value( Number( event.target.value).toFixed(2) );
+    }
+
     if( props.editable ) {
         return ( <div className={css.price}>
-            <input type='number' value={ props.value } required={true} onChange={props.onChange}/>
+            <input type='number' value={ value } required={true} onChange={onChange} onBlur={onBlur} />
         </div> );
     } else {
         return ( <div className={css.price}>
