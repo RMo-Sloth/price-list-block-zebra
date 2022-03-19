ea0e531339a69a0b0f547faef2e7af95da2187d8
Rob Monhemius <robmonhemius@gmail.com>
2022-03-19 19:48:39 +0100

    select deleteButton and UpdateRecord

--- src/components/price-records/delete/DeletePriceRecord.js
+++ src/components/price-records/delete/DeletePriceRecord.js
@@ -1,16 +1,24 @@
 import { Button, Icon } from '@wordpress/components';
+import { useEffect } from '@wordpress/element';
 import { trash } from '@wordpress/icons';
 import css from './DeletePriceRecord.module.scss';
 
 function DeletePriceRecord( props ) {
-    if( props.display === false ) return null;
+    if( props.display === false ) return null; // Do not render component
+
+    let button_ref;
+
+    useEffect( () => {
+        if( props.focus === true )
+            button_ref.focus();
+    }, [props.focus]);
 
     function emit() {
         props.onEmit();
     }
 
-    return ( <div className={css.delete}>
-        <Button isDestructive isPrimary isSmall onClick={emit} >
+    return ( <div className={css.delete} >
+        <Button isDestructive isPrimary isSmall onClick={emit} ref = { ref => button_ref = ref } >
             <Icon icon={ trash } size='20' />
         </Button>
     </div> );
--- src/components/price-records/update/price-input/PriceInput.js
+++ src/components/price-records/update/price-input/PriceInput.js
@@ -2,12 +2,17 @@ import css from './PriceInput.module.scss';
 import { useState, useEffect } from '@wordpress/element';
 
 export default function PriceInput( props ) {
-
+    let input_ref;
     const [ value, set_value ] = useState( props.value );
 
     useEffect( () => {
         props.onChange( Number( value ).toFixed(2) );
     }, [ value ]);
+
+    useEffect( () => {
+        if( props.focus )
+            input_ref.select();
+    }, [ props.focus ]);
 
     function onChange( event ) {
         set_value( event.target.value );
@@ -19,7 +24,7 @@ export default function PriceInput( props ) {
 
     if( props.editable ) {
         return ( <div className={css.price}>
-            <input type='number' value={ value } required={true} onChange={onChange} onBlur={onBlur} />
+            <input type='number' value={ value } required={true} onChange={onChange} onBlur={onBlur} ref={ ref => input_ref = ref }/>
         </div> );
     } else {
         return ( <div className={css.price}>
