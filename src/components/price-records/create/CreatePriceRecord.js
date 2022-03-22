c58be8fb8cbd56bf27b62f7fa9081663c4bb686c
Rob Monhemius <robmonhemius@gmail.com>
2022-03-22 17:44:21 +0100

    refactor: encapsulate reset_record

--- src/components/price-records/create/CreatePriceRecord.js
+++ src/components/price-records/create/CreatePriceRecord.js
@@ -5,17 +5,21 @@ import css from './CreatePriceRecord.module.scss';
 import { plus } from '@wordpress/icons';
 import PriceRecordContext from '../../../context/PriceRecordContext';
 
-function CreatePriceRecord( props ) {
+function CreatePriceRecord() {
 
     const [record, set_record] = useState( { name: '', price: '' } );
     const name_input_ref = createRef();
     const records = useContext( PriceRecordContext );
     
-    function emit(){
-        const price = Number( record.price ).toFixed(2);
-        records.add( {...record, price } );
+    function reset_record() {
         set_record( { name: '', price: '' } );
         name_input_ref.current.focus();
+    }
+
+    function add() {
+        const price = Number( record.price ).toFixed(2);
+        records.add( {...record, price } );
+        reset_record();
     }
 
     function set_name( event ) {
@@ -35,7 +39,7 @@ function CreatePriceRecord( props ) {
         <div className={css.price}>
             <input type='number' placeholder='0.00' value={record.price} onChange={set_price} />
         </div>
-        <Button isPrimary isSmall onClick={emit} className={css.insert}>
+        <Button isPrimary isSmall onClick={add} className={css.insert}>
             <Icon icon={plus} size='20' />
         </Button>
     </div> );
