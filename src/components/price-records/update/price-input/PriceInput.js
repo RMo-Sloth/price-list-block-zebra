bd5a1b15e9286498b0bef891c86c91b00e762879
Rob Monhemius <robmonhemius@gmail.com>
2023-02-13 15:35:13 +0100

    compiled to js-lint standards

--- src/components/price-records/update/price-input/PriceInput.js
+++ src/components/price-records/update/price-input/PriceInput.js
@@ -1,44 +1,42 @@
 import css from './PriceInput.module.scss';
 import { useState, useEffect } from '@wordpress/element';
 
-export default function PriceInput(props) {
-	let input_ref;
-	const [value, set_value] = useState(props.value);
+export default function PriceInput( props ) {
+	let inputRef;
+	const [ value, setValue ] = useState( props.value );
 
-	useEffect(() => {
-		props.onChange(Number(value).toFixed(2));
-	}, [value]);
+	useEffect( () => {
+		props.onChange( Number( value ).toFixed( 2 ) );
+	}, [ value ] );
 
-	useEffect(() => {
-		if (props.focus) input_ref.select();
-	}, [props.focus]);
+	useEffect( () => {
+		if ( props.focus ) inputRef.select();
+	}, [ props.focus ] );
 
-	function onChange(event) {
-		set_value(event.target.value);
+	function onChange( event ) {
+		setValue( event.target.value );
 	}
 
-	function onBlur(event) {
-		set_value(Number(event.target.value).toFixed(2));
+	function onBlur( event ) {
+		setValue( Number( event.target.value ).toFixed( 2 ) );
 	}
 
-	if (props.editable) {
+	if ( props.editable )
 		return (
-			<div className={css.price}>
+			<div className={ css.price }>
 				<input
 					type="number"
-					value={value}
-					required={true}
-					onChange={onChange}
-					onBlur={onBlur}
-					ref={(ref) => (input_ref = ref)}
+					value={ value }
+					required={ true }
+					onChange={ onChange }
+					onBlur={ onBlur }
+					ref={ ( ref ) => ( inputRef = ref ) }
 				/>
 			</div>
 		);
-	} else {
-		return (
-			<div className={css.price}>
-				<span> {props.value} </span>
-			</div>
-		);
-	}
+	return (
+		<div className={ css.price }>
+			<span> { props.value } </span>
+		</div>
+	);
 }
