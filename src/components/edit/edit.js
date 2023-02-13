1d2dd4f964a9b69554c9d9acf7631fadd1ceb50d
Rob Monhemius <robmonhemius@gmail.com>
2023-02-13 14:04:54 +0100

    complied to js-lint standards

--- src/components/edit/edit.js
+++ src/components/edit/edit.js
@@ -7,63 +7,63 @@ import { __ } from '@wordpress/i18n';
 import style from './editor.module.scss';
 import PriceRecordContext from '../../context/PriceRecordContext';
 
-export default function Edit({ attributes, setAttributes }) {
-	const [focus_data, set_focus_data] = useState({
+export default function Edit( { attributes } ) {
+	const [ focusData, setFocusData ] = useState( {
 		focus_on: null,
 		initial_first_record: null,
-	});
+	} );
 
-	useEffect(set_initial_focus, []);
+	useEffect( SetInitialFocus, [] );
 
-	function set_initial_focus() {
-		if (attributes.price_records.length === 0) return;
+	function SetInitialFocus() {
+		if ( attributes.price_records.length === 0 ) return;
 		// do not select a 'new first-record' after adding / removing elements
-		set_focus_data({
+		setFocusData( {
 			focus_on: 'record',
-			initial_first_record: attributes.price_records[0].id,
-		});
+			initial_first_record: attributes.price_records[ 0 ].id,
+		} );
 	}
 
-	const price_records_manager = useContext(PriceRecordContext);
+	const priceRecordsManager = useContext( PriceRecordContext );
 
-	function create_price_record() {
-		if (attributes.settings.add) return <CreatePriceRecord />;
+	function createPriceRecord() {
+		if ( attributes.settings.add ) return <CreatePriceRecord />;
 	}
 
-	function action_label() {
-		if (attributes.settings.delete)
-			return <div className={style.action}></div>;
+	function actionLabel() {
+		if ( attributes.settings.delete )
+			return <div className={ style.action }></div>;
 	}
 
-	function order_label() {
-		if (attributes.settings.order_items)
-			return <div className={style.order}></div>;
+	function orderLabel() {
+		if ( attributes.settings.order_items )
+			return <div className={ style.order }></div>;
 	}
 
 	return (
 		<div>
-			<div className={style['price-record-top-labels']}>
-				{order_label()}
-				<div className={style.name}>
-					{__('Item / Service', 'price-list-block-zebra')}
+			<div className={ style[ 'price-record-top-labels' ] }>
+				{ orderLabel() }
+				<div className={ style.name }>
+					{ __( 'Item / Service', 'price-list-block-zebra' ) }
 				</div>
-				<div className={style.price}>
-					{__('Price', 'price-list-block-zebra')}
+				<div className={ style.price }>
+					{ __( 'Price', 'price-list-block-zebra' ) }
 				</div>
-				{action_label()}
+				{ actionLabel() }
 			</div>
-			{price_records_manager.records.map((record) => (
+			{ priceRecordsManager.records.map( ( record ) => (
 				<UpdatePriceRecord
-					key={record.id}
+					key={ record.id }
 					focus={
-						focus_data.initial_first_record === record.id &&
-						focus_data.focus_on === 'record'
+						focusData.initial_first_record === record.id &&
+						focusData.focus_on === 'record'
 					}
-					record={record}
-					settings={attributes.settings}
+					record={ record }
+					settings={ attributes.settings }
 				/>
-			))}
-			{create_price_record()}
+			) ) }
+			{ createPriceRecord() }
 		</div>
 	);
 }
