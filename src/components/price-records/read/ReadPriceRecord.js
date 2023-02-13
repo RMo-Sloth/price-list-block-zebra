371dc414a41b91c50b8ce78bb8f794c1c89474b3
Rob Monhemius <robmonhemius@gmail.com>
2023-02-13 14:59:30 +0100

    complied to js-lint standards

--- src/components/price-records/read/ReadPriceRecord.js
+++ src/components/price-records/read/ReadPriceRecord.js
@@ -1,25 +1,26 @@
 // @see https://developer.wordpress.org/block-editor/packages/packages-i18n/
 import css from './ReadPriceRecord.module.scss';
 
-function ReadPriceRecord(props) {
-	const name = props.record.name.split(' ');
-	const name_jsx = name.map((word, index) => {
-		if (name.length === index + 1)
+function ReadPriceRecord( props ) {
+	const name = props.record.name.split( ' ' );
+	const nameJsx = name.map( ( word, index ) => {
+		if ( name.length === index + 1 )
 			return (
-				<span className={css['last-word']} key={index}>
-					{word}
+				<span className={ css[ 'last-word' ] } key={ index }>
+					{ word }
 				</span>
 			);
-		else return <span key={index}>{word}&nbsp;</span>;
-	});
+
+		return <span key={ index }>{ word }&nbsp;</span>;
+	} );
 
 	return (
-		<div className={css['record-row']}>
-			<div className={css.name}>
-				{name_jsx}
-				<span className={css.spacer}></span>
+		<div className={ css[ 'record-row' ] }>
+			<div className={ css.name }>
+				{ nameJsx }
+				<span className={ css.spacer }></span>
 			</div>
-			<div className={css.price}>{props.record.price}</div>
+			<div className={ css.price }>{ props.record.price }</div>
 		</div>
 	);
 }
