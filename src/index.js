73c793c2bcf3f24ec86b61aa047e85e86839a682
Rob Monhemius <robmonhemius@gmail.com>
2023-02-14 19:33:07 +0100

    complied to js-lint standards

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { registerBlockType } from '@wordpress/blocks';
 import Save from './save';
 import Editor from './Editor';
 
-registerBlockType('price-list-block-zebra/price-list-block-zebra', {
+registerBlockType( 'price-list-block-zebra/price-list-block-zebra', {
 	edit: Editor,
 	save: Save,
-});
+} );
