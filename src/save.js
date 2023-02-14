8952aa9524e079141c35be96a4488dbe86e03f29
Rob Monhemius <robmonhemius@gmail.com>
2023-02-14 19:32:21 +0100

    complied to js-lint standards

--- src/save.js
+++ src/save.js
@@ -1,12 +1,12 @@
 import ReadPriceRecord from './components/price-records/read/ReadPriceRecord';
 import { useBlockProps } from '@wordpress/block-editor';
 
-export default function Save({ attributes }) {
+export default function Save( { attributes } ) {
 	return (
-		<div {...useBlockProps.save()}>
-			{attributes.price_records.map((record) => (
-				<ReadPriceRecord key={record.id} record={record} />
-			))}
+		<div { ...useBlockProps.save() }>
+			{ attributes.price_records.map( ( record ) => (
+				<ReadPriceRecord key={ record.id } record={ record } />
+			) ) }
 		</div>
 	);
 }
