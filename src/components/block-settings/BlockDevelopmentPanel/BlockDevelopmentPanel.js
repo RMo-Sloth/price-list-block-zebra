438302bdbf63077c1c14dcc0d97053be8614bb10
Rob Monhemius <robmonhemius@gmail.com>
2023-03-08 13:33:07 +0100

    extracted BlockDevelopmentPanel

---
+++ src/components/block-settings/BlockDevelopmentPanel/BlockDevelopmentPanel.js
@@ -0,0 +1,38 @@
+import { Panel, PanelBody } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import logo from '../../../assets/logo.png';
+
+export default function BlockDevelopmentPanel() {
+	return (
+		<Panel>
+			<PanelBody
+				title={ __( 'Block Development', 'price-list-block-zebra' ) }
+				icon="info-outline"
+				initialOpen={ false }
+			>
+				<p>
+					{ __(
+						'For questions about WordPress Block Development and professional Wordpress Block development, visit:',
+						'price-list-block-zebra'
+					) }
+				</p>
+				<a
+					href="https://waardwebsites.nl/contact"
+					style={ {
+						display: 'block',
+						margin: 'auto',
+						width: '75%',
+					} }
+				>
+					<img
+						src={ logo }
+						alt={ __(
+							'waardwebsites.nl logo',
+							'price-list-block-zebra'
+						) }
+					/>
+				</a>
+			</PanelBody>
+		</Panel>
+	);
+}
