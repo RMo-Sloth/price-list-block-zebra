15ad7dfc5361f64cfb88584a89e0f4980a26e3c0
Rob Monhemius <robmonhemius@gmail.com>
2023-03-10 19:21:03 +0100

    js to tsx

---
+++ src/components/price-records/update/order-button/OrderButton.tsx
@@ -0,0 +1,19 @@
+// @ts-ignore
+import style from './OrderButton.module.scss';
+import { useContext } from '@wordpress/element';
+import PriceRecordContext from '../../../../context/PriceRecordContext';
+import { MoveDownButton } from './move-down-button/MoveDownButton';
+import { MoveUpButton } from './move-up-button/MoveUpButton';
+import { PriceRecord } from '../../../../records/priceRecord/priceRecord';
+
+export default function OrderButton( { focus, display, record }: { focus: boolean, display: boolean, record: PriceRecord } ): JSX.Element {
+	const records_context = useContext( PriceRecordContext );
+
+	if ( ! display ) return null; // Aborts code, returns nothing
+	if ( records_context.records.length <= 1 ) return null; // Aborts code, returns nothing
+
+	return <div className={ style.order_button }>
+		<MoveUpButton focus={ focus } record={ record } />
+		<MoveDownButton record={ record } focus={ focus }/>
+	</div>;
+}
