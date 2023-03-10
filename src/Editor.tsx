3da252bc0fc70c60d94b4fabf6254db63da90d57
Rob Monhemius <robmonhemius@gmail.com>
2023-03-10 13:40:32 +0100

    refactor: cleaned up duplicate logic

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -3,46 +3,25 @@ import PreviewInEditor from './components/edit/editor-preview';
 import { SidePanel } from './components/SidePanel/SidePanel';
 import { useBlockProps } from '@wordpress/block-editor';
 import { PriceRecordContextProvider } from './context/PriceRecordContext';
+import { PriceRecord } from './records/priceRecord/priceRecord';
+import { SettingsRecord } from './records/settingsRecord/settingsRecord';
 
 export default function Editor( props: any ): JSX.Element {
-	const blockProps = useBlockProps();
+	const is_editable: boolean = props.isSelected || props.attributes.price_records.length === 0;
 
-	function updateSettings( settings: any ): void {
+	function updateSettings( settings: SettingsRecord ): void {
 		props.setAttributes( { ...props.attributes, settings } );
 	}
 
-	function updateRecords( records: any ): void {
+	function updateRecords( records: PriceRecord[] ): void {
 		props.setAttributes( { ...props.attributes, price_records: records } );
 	}
 
-	if ( props.isSelected || props.attributes.price_records.length === 0 ) {
-		return (
-			<div { ...blockProps }>
-				<PriceRecordContextProvider
-					records={ props.attributes.price_records }
-					on_save={ updateRecords }
-				>
-					<Edit { ...props } />
-				</PriceRecordContextProvider>
-				<SidePanel
-					settings={ props.attributes.settings }
-					onChange={ updateSettings }
-				/>
-			</div>
-		);
-	}
-	return (
-		<div { ...blockProps }>
-			<PriceRecordContextProvider
-				records={ props.attributes.price_records }
-				on_save={ updateRecords }
-			>
-				<PreviewInEditor { ...props } />
-			</PriceRecordContextProvider>
-			<SidePanel
-				settings={ props.attributes.settings }
-				onChange={ updateSettings }
-			/>
-		</div>
-	);
+
+	return <div { ...useBlockProps() }>
+		<PriceRecordContextProvider records={ props.attributes.price_records } on_save={ updateRecords } >
+			{ is_editable ? <Edit { ...props } /> : <PreviewInEditor { ...props } /> }
+		</PriceRecordContextProvider>
+		<SidePanel settings={ props.attributes.settings } onChange={ updateSettings } />
+	</div>;
 }
