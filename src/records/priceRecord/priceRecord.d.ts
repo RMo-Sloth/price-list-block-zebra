711c1d005dea94ee353f0df29da766e3b32b2dcd
Rob Monhemius <robmonhemius@gmail.com>
2023-03-12 13:49:24 +0100

    typo

--- src/records/priceRecord/priceRecord.d.ts
+++ src/records/priceRecord/priceRecord.d.ts
@@ -2,7 +2,7 @@ export interface PriceRecord {
 	/** The name of the product */
 	name: string;
 	
-	/** The price of the proodct */
+	/** The price of the product */
 	price: number;
 	
 	/** An unique record identifier */
