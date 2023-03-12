3aeda931a2a4fc6f9e216348e745c327ef4f6b89
Rob Monhemius <robmonhemius@gmail.com>
2023-03-12 13:20:20 +0100

    refactor: extracted type

--- src/records/priceRecord/PriceRecordContext.tsx
+++ src/records/priceRecord/PriceRecordContext.tsx
@@ -1,20 +1,7 @@
 import { createContext, useEffect, useState } from '@wordpress/element';
 import { PriceRecordsFunctions } from './PriceRecordsFunctions';
 import { PriceRecord } from "./PriceRecord";
-
-interface PriceRecordContextAPI {
-	records: PriceRecord[];
-	/** Add a new record to the records stored in this Context. */
-	add: (record: PriceRecord) => void;
-	/** Replace a record in the records stored in this Context. */
-	update: (record: PriceRecord) => void;
-	/** Remove a record from the records stored in this Context. */
-	remove: (record: PriceRecord) => void;
-	/** Move down a record inside the records array stored in this Context. */
-	move_down: (record: PriceRecord) => void;
-	/** Move up a record inside the records array stored in this Context. */
-	move_up: (record: PriceRecord) => void;
-}
+import { PriceRecordContextAPI } from './PriceRecordContextAPI';
 
 const PriceRecordContext = createContext<PriceRecordContextAPI>({
 	records: [],
---
+++ src/records/priceRecord/PriceRecordContextAPI.d.ts
@@ -0,0 +1,21 @@
+import { PriceRecord } from "./PriceRecord";
+
+export interface PriceRecordContextAPI {
+	/** The state supplied by the context */
+	records: PriceRecord[];
+	
+	/** Add a new record to the records stored in this Context. */
+	add: (record: PriceRecord) => void;
+	
+	/** Replace a record in the records stored in this Context. */
+	update: (record: PriceRecord) => void;
+	
+	/** Remove a record from the records stored in this Context. */
+	remove: (record: PriceRecord) => void;
+	
+	/** Move down a record inside the records array stored in this Context. */
+	move_down: (record: PriceRecord) => void;
+	
+	/** Move up a record inside the records array stored in this Context. */
+	move_up: (record: PriceRecord) => void;
+}
