d50d1f5c695936071eedf39353c95e0466268125
Rob Monhemius <robmonhemius@gmail.com>
2023-03-13 14:06:20 +0100

    moved Save component

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
-// @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-registration/
 import { registerBlockType } from '@wordpress/blocks';
-// @see https://www.npmjs.com/package/@wordpress/scripts#using-css
-
-// Internal dependencies
-import Save from './Save';
+import Save from './components/Save/Save';
 import Editor from './Editor';
 
 registerBlockType( 'price-list-block-zebra/price-list-block-zebra', {
