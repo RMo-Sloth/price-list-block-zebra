fc4539931f74ba2c67a49650a76e33ae7839c911
Rob Monhemius <robmonhemius@gmail.com>
2023-03-13 23:11:58 +0100

    handle focus after deletion

--- src/libraries/PriceRecord/components/update/delete/DeletePriceRecord.tsx
+++ src/libraries/PriceRecord/components/update/delete/DeletePriceRecord.tsx
@@ -2,21 +2,33 @@ import { Button } from '@wordpress/components';
 import { useEffect, useContext, useRef } from '@wordpress/element';
 import { trash } from '@wordpress/icons';
 import FocusContext from '../../../data/Focus/FocusContext';
+import { PriceRecord } from '../../../data/PriceRecord/PriceRecord';
 import PriceRecordContext from '../../../data/PriceRecord/PriceRecordsContext';
+import { PriceRecordsFunctions } from '../../../data/PriceRecord/PriceRecordsFunctions';
 import SettingsContext from '../../../data/Settings/SettingsContext';
 
 function DeletePriceRecord(props): JSX.Element {
 	const ref = useRef<HTMLButtonElement>(null);
 	const records_context = useContext(PriceRecordContext);
 	const settings = useContext(SettingsContext);
-	const {focusEvent} = useContext(FocusContext);
+	const { focusEvent, setFocusEvent } = useContext(FocusContext);
 
 	useEffect(() => {
-		if ( ref.current && focusEvent.name === 'focus_delete' && focusEvent.options.record_index === props.record.index )
+		console.log(focusEvent)
+		if (ref.current && focusEvent.name === 'focus_delete' && focusEvent.options.record_index === props.record.index)
 			ref.current.focus();
 	}, [focusEvent]);
 
 	function remove(): void {
+		const next_record: PriceRecord = PriceRecordsFunctions.nextRecord(records_context.records, props.record);
+		const previous_record: PriceRecord = PriceRecordsFunctions.previousRecord(records_context.records, props.record);
+		if (next_record)
+			setFocusEvent({ name: 'focus_delete', options: { record_index: next_record.index } })
+		else if (previous_record)
+			setFocusEvent({ name: 'focus_delete', options: { record_index: previous_record.index } })
+		else if (settings.add)
+			setFocusEvent({ name: 'select_new_record', options: null })
+
 		records_context.remove(props.record);
 	}
 
--- src/libraries/PriceRecord/data/PriceRecord/PriceRecordsFunctions.ts
+++ src/libraries/PriceRecord/data/PriceRecord/PriceRecordsFunctions.ts
@@ -1,64 +1,74 @@
 import { PriceRecord } from "./PriceRecord";
 
 export class PriceRecordsFunctions {
-	static add( records: PriceRecord[], record: PriceRecord ): PriceRecord[] {
-		const enhancedRecord = { ...record, index: PriceRecordsFunctions.latestId( records ) + 1 };
-		return [ ...records, enhancedRecord ];
+	static add(records: PriceRecord[], record: PriceRecord): PriceRecord[] {
+		const enhancedRecord = { ...record, index: PriceRecordsFunctions.latestId(records) + 1 };
+		return [...records, enhancedRecord];
 	}
 
-	static update( records: PriceRecord[], updated_record: PriceRecord ): PriceRecord[] {
-		const new_records = [ ...records ];
+	static update(records: PriceRecord[], updated_record: PriceRecord): PriceRecord[] {
+		const new_records = [...records];
 		const index = records.findIndex(
-			( record ) => record.index === updated_record.index
+			(record) => record.index === updated_record.index
 		);
-		new_records[ index ] = updated_record;
+		new_records[index] = updated_record;
 		return new_records;
 	}
 
-	static isFirst( records: PriceRecord[], record: PriceRecord ): boolean {
-		return PriceRecordsFunctions.indexOf( records, record ) === 0;
+	static isFirst(records: PriceRecord[], record: PriceRecord): boolean {
+		return PriceRecordsFunctions.indexOf(records, record) === 0;
 	}
 
-	static isLast( records: PriceRecord[], record: PriceRecord ): boolean {
-		return PriceRecordsFunctions.indexOf( records, record ) + 1 === records.length;
+	static isLast(records: PriceRecord[], record: PriceRecord): boolean {
+		return PriceRecordsFunctions.indexOf(records, record) + 1 === records.length;
 	}
 
-	static remove( records: PriceRecord[], removed_record: PriceRecord ): PriceRecord[] {
+	static remove(records: PriceRecord[], removed_record: PriceRecord): PriceRecord[] {
 		return records.filter(
-			( record ) => record.index !== removed_record.index
+			(record) => record.index !== removed_record.index
 		);
 	}
 
-	static moveDown( records: PriceRecord[], record: PriceRecord ): PriceRecord[] {
-		const index = PriceRecordsFunctions.indexOf( records, record );
-		return PriceRecordsFunctions.swapPlaces( records, index, index + 1 );
+	static moveDown(records: PriceRecord[], record: PriceRecord): PriceRecord[] {
+		const index = PriceRecordsFunctions.indexOf(records, record);
+		return PriceRecordsFunctions.swapPlaces(records, index, index + 1);
 	}
 
-	static moveUp( records: PriceRecord[], record: PriceRecord ): PriceRecord[] {
-		const index = PriceRecordsFunctions.indexOf( records, record );
-		return PriceRecordsFunctions.swapPlaces( records, index, index - 1 );
+	static moveUp(records: PriceRecord[], record: PriceRecord): PriceRecord[] {
+		const index = PriceRecordsFunctions.indexOf(records, record);
+		return PriceRecordsFunctions.swapPlaces(records, index, index - 1);
 	}
 
-	private static indexOf( records: PriceRecord[], targetRecord: PriceRecord ): number {
+	static nextRecord(records: PriceRecord[], record: PriceRecord): PriceRecord | undefined {
+		const index = PriceRecordsFunctions.indexOf(records, record);
+		return records[index + 1];
+	}
+
+	static previousRecord(records: PriceRecord[], record: PriceRecord): PriceRecord | undefined {
+		const index = PriceRecordsFunctions.indexOf(records, record);
+		return records[index - 1];
+	}
+
+	private static indexOf(records: PriceRecord[], targetRecord: PriceRecord): number {
 		return records.findIndex(
-			( comparedRecord ) => comparedRecord === targetRecord
+			(comparedRecord) => comparedRecord === targetRecord
 		);
 	}
 
-	private static latestId( records: PriceRecord[] ) {
+	private static latestId(records: PriceRecord[]) {
 		return records.reduce(
-			( prev, current ) => Math.max( prev, current.index ),
+			(prev, current) => Math.max(prev, current.index),
 			0
 		);
 	}
 
-	private static swapPlaces( records: PriceRecord[], index1: number, index2: number ): PriceRecord[] {
-		const priceRecords = [ ...records ];
-		const record1 = priceRecords[ index1 ];
-		const record2 = priceRecords[ index2 ];
+	private static swapPlaces(records: PriceRecord[], index1: number, index2: number): PriceRecord[] {
+		const priceRecords = [...records];
+		const record1 = priceRecords[index1];
+		const record2 = priceRecords[index2];
 
-		priceRecords[ index1 ] = record2;
-		priceRecords[ index2 ] = record1;
+		priceRecords[index1] = record2;
+		priceRecords[index2] = record1;
 
 		return priceRecords;
 	}
