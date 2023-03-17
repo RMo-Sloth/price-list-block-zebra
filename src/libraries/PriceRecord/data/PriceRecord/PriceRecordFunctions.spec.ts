e71d1c76afeadb2f2c99dd6d64e28a7cd72b6eaf
Rob Monhemius <robmonhemius@gmail.com>
2023-03-17 17:01:44 +0100

    implemented set_price method

--- src/libraries/PriceRecord/data/PriceRecord/PriceRecordFunctions.spec.ts
+++ src/libraries/PriceRecord/data/PriceRecord/PriceRecordFunctions.spec.ts
@@ -35,7 +35,42 @@ describe('PriceRecordFunctions', () => {
 		test('returns a newly created object reference', () => {
 			const default_record = PriceRecordFunctions.default();
 			const new_record = PriceRecordFunctions.set_name(default_record, 'I am a fictional name');
-			expect( Object.is( default_record, new_record) ).toBe(false);
+			expect(Object.is(default_record, new_record)).toBe(false);
+		});
+
+	});
+
+
+	describe('.set_price ', () => {
+
+		test('sets a new price', () => {
+			const default_record = PriceRecordFunctions.default();
+			const new_record = PriceRecordFunctions.set_price(default_record, 12.35);
+			expect(new_record.price).toBe(12.35);
+		});
+
+		test('does not allow prices below 0', () => {
+			const default_record = PriceRecordFunctions.default();
+			const new_record = PriceRecordFunctions.set_price(default_record, -12.35);
+			expect(new_record.price).toBe(0);
+		});
+
+		test('allows for 2 digits', () => {
+			const default_record = PriceRecordFunctions.default();
+			const new_record = PriceRecordFunctions.set_price(default_record, 512.189);
+			expect(new_record.price).toBe(512.18);
+		});
+
+		test('returns an immutable object', () => {
+			const default_record = PriceRecordFunctions.default();
+			const new_record = PriceRecordFunctions.set_price(default_record, 12);
+			expect(Object.isSealed(new_record)).toBe(true);
+		});
+
+		test('returns a newly created object reference', () => {
+			const default_record = PriceRecordFunctions.default();
+			const new_record = PriceRecordFunctions.set_price(default_record, 10);
+			expect(Object.is(default_record, new_record)).toBe(false);
 		});
 
 	});
--- src/libraries/PriceRecord/data/PriceRecord/PriceRecordFunctions.ts
+++ src/libraries/PriceRecord/data/PriceRecord/PriceRecordFunctions.ts
@@ -12,4 +12,12 @@ export class PriceRecordFunctions {
 		return Object.seal(result)
 	}
 
+	static set_price( record: PriceRecord, price: number ): PriceRecord {
+		price = price < 0 ? 0: price;
+		price = price % 0.01 > 0 ? Math.floor(price*100) / 100 : price;
+
+		const result = { ...record, price };
+		return Object.seal( result );
+	}
+
 }
