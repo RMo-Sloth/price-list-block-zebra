0d355ac29a5aa1beeb9f55546df8bbc567b54d06
Rob Monhemius <robmonhemius@gmail.com>
2023-03-20 11:32:33 +0100

    created find_by_id method

--- src/libraries/PriceRecord/data/PriceRecordCollection/PriceRecordCollectionFunctions.ts
+++ src/libraries/PriceRecord/data/PriceRecordCollection/PriceRecordCollectionFunctions.ts
@@ -11,8 +11,8 @@ export class PriceRecordCollectionFunctions {
 	}
 
 	static update(records: PriceRecordCollection, updated_record: PriceRecord): PriceRecordCollection {
-		const index = PriceRecordCollectionFunctions.indexOf(records, updated_record);
-		const new_records = PriceRecordCollectionFunctions.replace( records, records[index], updated_record );
+		const old_record = PriceRecordCollectionFunctions.find_by_id(records, updated_record);
+		const new_records = PriceRecordCollectionFunctions.replace( records, old_record, updated_record );
 		return Object.freeze(new_records);
 	}
 
@@ -51,15 +51,19 @@ export class PriceRecordCollectionFunctions {
 		return records[index - 1];
 	}
 
+	private static find_by_id( records: PriceRecordCollection, record: PriceRecord ): PriceRecord {
+		return records.find( ({index}) => index === record.index );
+	}
+
 	private static indexOf(records: PriceRecordCollection, record: PriceRecord): number {
 		return records.findIndex(({ index }) => index === record.index);
 	}
 
-	private static latestId(records: PriceRecordCollection) {
+	private static latestId(records: PriceRecordCollection): number {
 		return records.reduce((prev, current) => Math.max(prev, current.index), 0);
 	}
 
-	/** Replaces records that exists. If one record matches it will be replaced by the other one. When both records match they will switch in the array. When no records match nothing will happen */
+	/** Replaces records that exists. If one record matches it will be replaced by the other one. When both records match they will swap places. When no records match nothing will happen */
 	private static replace(records: PriceRecordCollection, record_1: PriceRecord, record_2: PriceRecord): PriceRecordCollection {
 		// NOTE: the records are matched by reference, so objects with the same values and different reference won't match.
 		return records.map(record => {
