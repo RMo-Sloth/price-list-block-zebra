e1b629bb3abcfce5be595542cc3e340a691d335a
Rob Monhemius <robmonhemius@gmail.com>
2023-03-20 11:40:52 +0100

    refactor: simplified logic

--- src/libraries/PriceRecord/data/PriceRecord/PriceRecordFunctions.ts
+++ src/libraries/PriceRecord/data/PriceRecord/PriceRecordFunctions.ts
@@ -12,17 +12,19 @@ export class PriceRecordFunctions {
 		return Object.freeze(result)
 	}
 
-	static set_price( record: PriceRecord, price: number ): PriceRecord {
-		price = price < 0 ? 0: price;
-		price = price % 0.01 > 0 ? Math.floor(price*100) / 100 : price;
+	static set_price(record: PriceRecord, price: number): PriceRecord {
+		if (price < 0)
+			price = 0;
+		else if (price % 0.01 > 0)
+			price = Math.floor(price * 100) / 100;
 
 		const result = { ...record, price };
-		return Object.freeze( result );
+		return Object.freeze(result);
 	}
 
 	static set_index(record: PriceRecord, index: number) {
 		const result = { ...record, index };
-		return Object.freeze( result );
+		return Object.freeze(result);
 	}
 
 }
