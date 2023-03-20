8bce794776722d4c5a2d300335f578a4b6658407
Rob Monhemius <robmonhemius@gmail.com>
2023-03-20 10:08:08 +0100

    formatted text

--- src/libraries/Promotion/components/BlockDevelopmentPanel/BlockDevelopmentPanel.tsx
+++ src/libraries/Promotion/components/BlockDevelopmentPanel/BlockDevelopmentPanel.tsx
@@ -6,33 +6,18 @@ import logo from '../../assets/logo.png';
 export default function BlockDevelopmentPanel() {
 	return <Panel>
 		<PanelBody
-			title={ __( 'Block Development', 'price-list-block-zebra' ) }
+			title={__('Block Development', 'price-list-block-zebra')}
 			icon="info-outline"
-			initialOpen={ false }
+			initialOpen={false}
 		>
-			<p>
-				{ __(
-					'For questions about WordPress Block Development and professional Wordpress Block development, visit:',
-					'price-list-block-zebra'
-				) }
-			</p>
+			<p>{__('For questions about WordPress Block Development and professional Wordpress Block development, visit:', 'price-list-block-zebra')}</p>
 			<a
-				href={ __(
-					'https://waardwebsites.nl/contact',
-					'price-list-block-zebra'
-				) }
-				style={ {
-					display: 'block',
-					margin: 'auto',
-					width: '75%',
-				} }
+				href={__('https://waardwebsites.nl/contact', 'price-list-block-zebra')}
+				style={{ display: 'block', margin: 'auto', width: '75%', }}
 			>
 				<img
-					src={ logo }
-					alt={ __(
-						'waardwebsites.nl logo',
-						'price-list-block-zebra'
-					) }
+					src={logo}
+					alt={__('waardwebsites.nl logo', 'price-list-block-zebra')}
 				/>
 			</a>
 		</PanelBody>
