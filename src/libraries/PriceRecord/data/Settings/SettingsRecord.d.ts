abe63b09d834b09b55ea20c0abe6fc162e5e9c71
Rob Monhemius <robmonhemius@gmail.com>
2023-03-20 10:12:31 +0100

    enforced immutability by type

--- src/libraries/PriceRecord/data/Settings/SettingsRecord.d.ts
+++ src/libraries/PriceRecord/data/Settings/SettingsRecord.d.ts
@@ -1,16 +1,16 @@
 export interface SettingsRecord {
 	/** Enables adding new products */
-	add: boolean;
+	readonly add: boolean;
 
 	/** Enables removing new products */
-	delete: boolean;
+	readonly delete: boolean;
 
 	/** Enables editing of the product descriptiion */
-	edit_description: boolean;
+	readonly edit_description: boolean;
 
 	/** Enables editing of the product price */
-	edit_price: boolean;
+	readonly edit_price: boolean;
 
 	/** Enables changing the order in which the products appear */
-	order_items: boolean;
+	readonly order_items: boolean;
 }
