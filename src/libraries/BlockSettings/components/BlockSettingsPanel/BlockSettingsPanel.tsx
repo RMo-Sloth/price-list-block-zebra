b903d465a7ea57c3e29b8cdc0d807790363ecec9
Rob Monhemius <robmonhemius@gmail.com>
2023-03-20 10:05:05 +0100

    reformatted doc

--- src/libraries/BlockSettings/components/BlockSettingsPanel/BlockSettingsPanel.tsx
+++ src/libraries/BlockSettings/components/BlockSettingsPanel/BlockSettingsPanel.tsx
@@ -24,72 +24,42 @@ export default function BlockSettingsPanel({ settings, onChange }: Props): JSX.E
 
 	return <Panel>
 		<PanelBody
-			title={__(
-				'Editing Experience',
-				'price-list-block-zebra'
-			)}
+			title={__('Editing Experience', 'price-list-block-zebra')}
 			icon="edit"
 			initialOpen={false}
 		>
 			<PanelRow>
 				<CheckboxControl
-					label={__(
-						'Delete Items',
-						'price-list-block-zebra'
-					)}
+					label={__('Delete Items', 'price-list-block-zebra')}
 					checked={local_settings.delete}
-					onChange={() => {
-						toggleSetting('delete')
-					}}
+					onChange={() => { toggleSetting('delete') }}
 				/>
 			</PanelRow>
 			<PanelRow>
 				<CheckboxControl
-					label={__(
-						'Add Items',
-						'price-list-block-zebra'
-					)}
+					label={__('Add Items', 'price-list-block-zebra')}
 					checked={local_settings.add}
-					onChange={() => {
-						toggleSetting('add')
-					}}
+					onChange={() => { toggleSetting('add') }}
 				/>
 			</PanelRow>
 			<PanelRow>
 				<CheckboxControl
-					label={__(
-						'Edit Price',
-						'price-list-block-zebra'
-					)}
+					label={__('Edit Price', 'price-list-block-zebra')}
 					checked={local_settings.edit_price}
-					onChange={() => {
-						toggleSetting('edit_price')
-					}}
+					onChange={() => { toggleSetting('edit_price') }}
 				/>
 			</PanelRow>
 			<PanelRow>
 				<CheckboxControl
-					label={__(
-						'Edit Description',
-						'price-list-block-zebra'
-					)}
+					label={__('Edit Description', 'price-list-block-zebra')}
 					checked={local_settings.edit_description}
-					onChange={() => {
-						toggleSetting('edit_description')
-					}}
-				/>
+					onChange={() => { toggleSetting('edit_description') }} />
 			</PanelRow>
 			<PanelRow>
 				<CheckboxControl
-					label={__(
-						'Order Items',
-						'price-list-block-zebra'
-					)}
+					label={__('Order Items', 'price-list-block-zebra')}
 					checked={local_settings.order_items}
-					onChange={() => {
-						toggleSetting('order_items');
-					}}
-				/>
+					onChange={() => { toggleSetting('order_items'); }} />
 			</PanelRow>
 		</PanelBody>
 	</Panel>;
