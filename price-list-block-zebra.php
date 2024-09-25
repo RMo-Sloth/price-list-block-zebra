028c544639da02f7d5f08487d25ce962c52e7645
Rob Monhemius <robmonhemius@gmail.com>
2024-09-25 15:35:35 +0100

    modified version number

--- price-list-block-zebra.php
+++ price-list-block-zebra.php
@@ -4,7 +4,7 @@
  * Description:       A wordpress block for creating price lists. You can use it to create a menu for a restaurant, prices for a barber shop or anything like it. All editing is done inside the block editor. No confusing menus. Just easy to use!
  * Requires at least: 6.0
  * Requires PHP:      7.0
- * Version:           1.0.1
+ * Version:           1.0.2
  * Author:            Rob Monhemius
  * Author URI:		  https://waardwebsites.nl
  * Plugin URI: 		  https://pricelist-block.waardwebsites.nl/
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: robmo
 Donate Link: https://donate.stripe.com/8wMg1r5QEaOb2oodQQ
 Tags: block
 Tested up to: 6.1
-Stable tag: 1.0.1
+Stable tag: 1.0.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
