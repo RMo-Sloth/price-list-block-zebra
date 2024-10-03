efbdaeaa599d896f7a501ff7fa1cd060b4f5bc43
Rob Monhemius <robmonhemius@gmail.com>
2024-10-03 21:46:01 +0100

    removed BlockDevelopmentPanel

--- src/components/SidePanel/SidePanel.tsx
+++ src/components/SidePanel/SidePanel.tsx
@@ -2,7 +2,6 @@ import { InspectorControls } from '@wordpress/block-editor';
 import { __ } from '@wordpress/i18n';
 import { BlockSettingsRecord } from '../../libraries/BlockSettings/data/BlockSettingsRecord';
 import BlockSettingsPanel from '../../libraries/BlockSettings/components/BlockSettingsPanel/BlockSettingsPanel';
-import BlockDevelopmentPanel from '../../libraries/Promotion/components/BlockDevelopmentPanel/BlockDevelopmentPanel';
 
 type Props = {
 	settings: BlockSettingsRecord,
@@ -12,6 +11,5 @@ type Props = {
 export function SidePanel( { settings, onChange }: Props ): JSX.Element {
 	return <InspectorControls key="setting">
 		<BlockSettingsPanel settings={settings} onChange={onChange} />
-		<BlockDevelopmentPanel />
 	</InspectorControls>;
 }
